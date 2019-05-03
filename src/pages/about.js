541ee02173d1efab3e9401fb0dcad788857836c3
David Sabalete <dsabalete@gmail.com>
2019-05-03 15:00:42 +0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I currently work full-time on Edreams ODIGEO.</p>
+        </div>
+    )
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,13 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello.</h1>
+            <h2>I'm David, a full-stack developer living in beautiful Barcelona.</h2>
+            <p>Need a developer? <a href="/contact">Contact me.</a></p>
+        </div>
+    )
+}
+
+export default IndexPage
