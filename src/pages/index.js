3c34a1df318ce637786f4493b7b473cec806abda
David Sabalete <dsabalete@gmail.com>
2019-05-05 23:49:59 +0200

    Remove Need a developer

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home" />
             <h1>Hello.</h1>
             <h2>I'm David, a full-stack developer living in beautiful Barcelona.</h2>
-            <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
         </Layout>
     )
 }
